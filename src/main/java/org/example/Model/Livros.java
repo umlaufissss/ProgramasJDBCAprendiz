30102b416084d5048da4ca83ae7748fda5e8d943
umlaufissss <umlaufisadora@gmail.com>
2026-03-05 21:41:36 -0300

    feat: criação de atributos das classes livros e usuario

--- src/main/java/org/example/Model/Livros.java
+++ src/main/java/org/example/Model/Livros.java
@@ -4,4 +4,55 @@ public class Livros
 {
     private long id;
     private String nome_livro;
+    private String autor_livro;
+    private int ano_lancamento;
+    private String status_disponivel;
+
+    public Livros(String status_disponivel, long id, String nome_livro, String autor_livro, int ano_lancamento) {
+        this.status_disponivel = status_disponivel;
+        this.id = id;
+        this.nome_livro = nome_livro;
+        this.autor_livro = autor_livro;
+        this.ano_lancamento = ano_lancamento;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStatus_disponivel() {
+        return status_disponivel;
+    }
+
+    public void setStatus_disponivel(String status_disponivel) {
+        this.status_disponivel = status_disponivel;
+    }
+
+    public int getAno_lancamento() {
+        return ano_lancamento;
+    }
+
+    public void setAno_lancamento(int ano_lancamento) {
+        this.ano_lancamento = ano_lancamento;
+    }
+
+    public String getNome_livro() {
+        return nome_livro;
+    }
+
+    public void setNome_livro(String nome_livro) {
+        this.nome_livro = nome_livro;
+    }
+
+    public String getAutor_livro() {
+        return autor_livro;
+    }
+
+    public void setAutor_livro(String autor_livro) {
+        this.autor_livro = autor_livro;
+    }
 }
--- src/main/java/org/example/Model/Usuario.java
+++ src/main/java/org/example/Model/Usuario.java
@@ -1,4 +1,90 @@
 package org.example.Model;
 
-public class Usuario {
+import java.sql.Date;
+
+public class Usuario
+{
+    private long id;
+    private String nome_usuario;
+    private String telefone;
+    private String email;
+    private String senha;
+    private Date dataNascimento;
+    private String tipoUsuario;
+    private String cpf;
+
+    public Usuario(String cpf, String tipoUsuario, Date dataNascimento, String senha, String telefone, String email, String nome_usuario, long id) {
+        this.cpf = cpf;
+        this.tipoUsuario = tipoUsuario;
+        this.dataNascimento = dataNascimento;
+        this.senha = senha;
+        this.telefone = telefone;
+        this.email = email;
+        this.nome_usuario = nome_usuario;
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNome_usuario() {
+        return nome_usuario;
+    }
+
+    public void setNome_usuario(String nome_usuario) {
+        this.nome_usuario = nome_usuario;
+    }
 }
